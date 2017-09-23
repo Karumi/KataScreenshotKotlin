1775624bf5c10dbfe1ecc93ba5ba5c7081b5d333
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-09-23 17:27:14 +0200

    First Commit

---
+++ app/src/main/java/com/karumi/ui/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.karumi.ui.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.karumi.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
