a839a58106e5373d76aed51e0dd01b3e2e639f41
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-10-04 09:18:13 +0200

    Add first test screenshot.

--- app/src/androidTest/java/com/karumi/ui/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/karumi/ui/view/MainActivityTest.kt
@@ -1,18 +1,32 @@
 package com.karumi.ui.view
 
 import com.github.salomonbrys.kodein.Kodein.Module
+import com.github.salomonbrys.kodein.bind
+import com.github.salomonbrys.kodein.instance
+import com.karumi.data.repository.SuperHeroRepository
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
 
 class MainActivityTest : AcceptanceTest<MainActivity>(MainActivity::class.java) {
 
+    @Mock private lateinit var repository: SuperHeroRepository
+
     @Test
     fun showsEmptyCaseIfThereAreNoSuperHeroes() {
+        givenEmptySuperHeroes()
+
         val activity = startActivity()
 
         record(activity)
     }
 
+    private fun givenEmptySuperHeroes() {
+        `when`(repository.getAllSuperHeroes()).thenReturn(emptyList())
+    }
+
     override val testDependencies =
         Module(allowSilentOverride = true) {
+            bind<SuperHeroRepository>() with instance(repository)
         }
 }
