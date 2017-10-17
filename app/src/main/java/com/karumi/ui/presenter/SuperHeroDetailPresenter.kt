6725b7eb7ba30e62508d962668636507eb8cfd98
Sergio Gutierrez <sergio@gokarumi.com>
2017-10-17 18:14:31 +0200

    Implement weak references with a Delegated Property

--- app/src/main/java/com/karumi/ui/presenter/SuperHeroDetailPresenter.kt
+++ app/src/main/java/com/karumi/ui/presenter/SuperHeroDetailPresenter.kt
@@ -2,12 +2,12 @@ package com.karumi.ui.presenter
 
 import android.util.Log
 import co.metalab.asyncawait.async
+import com.karumi.common.weak
 import com.karumi.domain.model.SuperHero
 import com.karumi.domain.usecase.GetSuperHeroByName
 import com.karumi.ui.LifecycleSubscriber
 import org.funktionale.either.Either.Left
 import org.funktionale.either.Either.Right
-import java.lang.ref.WeakReference
 
 class SuperHeroDetailPresenter(
         view: View,
@@ -18,8 +18,7 @@ class SuperHeroDetailPresenter(
         private val TAG = "SuperHeroDetail"
     }
 
-    private val viewWeak: WeakReference<View> = WeakReference(view)
-    private fun view() = viewWeak.get()
+    private val view: View? by weak(view)
 
     private lateinit var name: String
 
@@ -27,21 +26,21 @@ class SuperHeroDetailPresenter(
         if (name != null) {
             this.name = name
         } else {
-            view()?.close()
+            view?.close()
         }
     }
 
     override fun update() {
-        view()?.showLoading()
+        view?.showLoading()
         refreshSuperHeroes()
     }
 
     private fun refreshSuperHeroes() = async {
         val result = await { getSuperHeroByName(name) }
-        view()?.hideLoading()
-        when {
-            result is Right -> view()?.showSuperHero(result.r)
-            result is Left -> Log.d(SuperHeroDetailPresenter.TAG, "There was an error")
+        view?.hideLoading()
+        when (result) {
+            is Right -> view?.showSuperHero(result.r)
+            is Left -> Log.d(SuperHeroDetailPresenter.TAG, "There was an error")
         }
     }
 
--- app/src/main/java/com/karumi/ui/presenter/SuperHeroesPresenter.kt
+++ app/src/main/java/com/karumi/ui/presenter/SuperHeroesPresenter.kt
@@ -2,12 +2,12 @@ package com.karumi.ui.presenter
 
 import android.util.Log
 import co.metalab.asyncawait.async
+import com.karumi.common.weak
 import com.karumi.domain.model.SuperHero
 import com.karumi.domain.usecase.GetSuperHeroes
 import com.karumi.ui.LifecycleSubscriber
 import org.funktionale.either.Either.Left
 import org.funktionale.either.Either.Right
-import java.lang.ref.WeakReference
 
 class SuperHeroesPresenter(
         view: View,
@@ -17,26 +17,24 @@ class SuperHeroesPresenter(
         private val TAG: String = "SuperHeroesPresenter"
     }
 
-    private val viewWeak: WeakReference<View> = WeakReference(view)
-
-    private fun view() = viewWeak.get()
+    private val view: View? by weak(view)
 
     override fun update() {
-        view()?.showLoading()
+        view?.showLoading()
         refreshSuperHeroes()
     }
 
     private fun refreshSuperHeroes() = async {
         val result = await { getSuperHeroes() }
-        view()?.hideLoading()
+        view?.hideLoading()
         when {
-            result is Right && result.r.isEmpty() -> view()?.showEmptyCase()
-            result is Right && result.r.isNotEmpty() -> view()?.showSuperHeroes(result.r)
+            result is Right && result.r.isEmpty() -> view?.showEmptyCase()
+            result is Right && result.r.isNotEmpty() -> view?.showSuperHeroes(result.r)
             result is Left -> Log.d(TAG, "There was an error")
         }
     }
 
-    fun onSuperHeroClicked(superHero: SuperHero) = view()?.openDetail(superHero.name)
+    fun onSuperHeroClicked(superHero: SuperHero) = view?.openDetail(superHero.name)
 
     interface View {
         fun hideLoading()
