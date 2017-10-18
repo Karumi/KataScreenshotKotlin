3e011cf45238dc8a829670edeb3ad54e0ddafbd3
Sergio Gutierrez <sergio@gokarumi.com>
2017-10-18 12:26:48 +0200

    Remove either dependency and stop handling errors

--- app/src/main/java/com/karumi/domain/usecase/GetSuperHeroByName.kt
+++ app/src/main/java/com/karumi/domain/usecase/GetSuperHeroByName.kt
@@ -2,10 +2,8 @@ package com.karumi.domain.usecase
 
 import com.karumi.data.repository.SuperHeroRepository
 import com.karumi.domain.model.SuperHero
-import org.funktionale.either.Either
 
-class GetSuperHeroByName(val superHeroesRepository: SuperHeroRepository) {
+class GetSuperHeroByName(private val superHeroesRepository: SuperHeroRepository) {
 
-    operator fun invoke(name: String): Either<Error, SuperHero> =
-        Either.right(superHeroesRepository.getByName(name))
+    operator fun invoke(name: String): SuperHero = superHeroesRepository.getByName(name)
 }
--- app/src/main/java/com/karumi/ui/presenter/SuperHeroDetailPresenter.kt
+++ app/src/main/java/com/karumi/ui/presenter/SuperHeroDetailPresenter.kt
@@ -1,22 +1,15 @@
 package com.karumi.ui.presenter
 
-import android.util.Log
 import co.metalab.asyncawait.async
 import com.karumi.common.weak
 import com.karumi.domain.model.SuperHero
 import com.karumi.domain.usecase.GetSuperHeroByName
 import com.karumi.ui.LifecycleSubscriber
-import org.funktionale.either.Either.Left
-import org.funktionale.either.Either.Right
 
 class SuperHeroDetailPresenter(
         view: View,
         private val getSuperHeroByName: GetSuperHeroByName) :
         LifecycleSubscriber {
-
-    companion object {
-        private val TAG = "SuperHeroDetail"
-    }
 
     private val view: View? by weak(view)
 
@@ -38,10 +31,7 @@ class SuperHeroDetailPresenter(
     private fun refreshSuperHeroes() = async {
         val result = await { getSuperHeroByName(name) }
         view?.hideLoading()
-        when (result) {
-            is Right -> view?.showSuperHero(result.r)
-            is Left -> Log.d(SuperHeroDetailPresenter.TAG, "There was an error")
-        }
+        view?.showSuperHero(result)
     }
 
     interface View {
