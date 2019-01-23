e92aad6329b92c25c9419f28656827c1cc28ba1c
Sergio Gutiérrez <Serchinastico@users.noreply.github.com>
2019-01-23 19:03:50 +0100

    Update dependencies (#10)

--- app/src/androidTest/java/com/karumi/mockito/MockitoExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.karumi.mockito
-
-import org.mockito.Mockito
-import org.mockito.stubbing.OngoingStubbing
-
-object MockitoExtensions {
-    @JvmStatic
-    fun <T> on(methodCall: T): OngoingStubbing<T> = Mockito.`when`(methodCall)
-}
