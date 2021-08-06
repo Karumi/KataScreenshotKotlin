0e6165fb35cf1e99f5d212e000ed1dad892875e0
flipper83 <jorge@karumi.com>
2021-08-06 10:17:42 +0200

    remove some exercices from the example.

--- app/src/androidTest/java/com/karumi/ui/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/karumi/ui/view/MainActivityTest.kt
@@ -27,62 +27,6 @@ class MainActivityTest : AcceptanceTest<MainActivity>(MainActivity::class.java) 
         compareScreenshot(activity)
     }
 
-    @Test
-    fun showsJustOneSuperHero() {
-        givenThereAreSomeSuperHeroes(1)
-
-        val activity = startActivity()
-
-        compareScreenshot(activity)
-    }
-
-    @Test
-    fun showsSuperHeroesIfThereAreSomeSuperHeroes() {
-        givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES)
-
-        val activity = startActivity()
-
-        compareScreenshot(activity)
-    }
-
-    @Test
-    fun showsAvengersBadgeIfASuperHeroIsPartOfTheAvengersTeam() {
-        givenThereAreSomeAvengers(ANY_NUMBER_OF_SUPER_HEROES)
-
-        val activity = startActivity()
-
-        compareScreenshot(activity)
-    }
-
-    @Test
-    fun doesNotShowAvengersBadgeIfASuperHeroIsNotPartOfTheAvengersTeam() {
-        givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES)
-
-        val activity = startActivity()
-
-        compareScreenshot(activity)
-    }
-
-    private fun givenThereAreSomeAvengers(numberOfAvengers: Int): List<SuperHero> =
-        givenThereAreSomeSuperHeroes(numberOfAvengers, avengers = true)
-
-    private fun givenThereAreSomeSuperHeroes(
-        numberOfSuperHeroes: Int = 1,
-        avengers: Boolean = false
-    ): List<SuperHero> {
-        val superHeroes = IntRange(0, numberOfSuperHeroes - 1).map { id ->
-            val superHeroName = "SuperHero - $id"
-            val superHeroDescription = "Description Super Hero - $id"
-            SuperHero(
-                superHeroName, null, avengers,
-                superHeroDescription
-            )
-        }
-
-        whenever(repository.getAllSuperHeroes()).thenReturn(superHeroes)
-        return superHeroes
-    }
-
     private fun givenThereAreNoSuperHeroes() {
         whenever(repository.getAllSuperHeroes()).thenReturn(emptyList())
     }
--- app/src/androidTest/java/com/karumi/ui/view/SuperHeroViewHolderTest.kt
+++ app/src/androidTest/java/com/karumi/ui/view/SuperHeroViewHolderTest.kt
@@ -21,36 +21,7 @@ class SuperHeroViewHolderTest : ScreenshotTest {
         holder.render(superHero)
         compareScreenshot(holder.itemView, widthInPx = R.dimen.super_hero_row_height.toDim())
     }
-
-    @Test
-    fun showsSuperHeroesWithLongNames() {
-        val superHero = givenASuperHeroWithALongName()
-        val holder = givenASuperHeroViewHolder()
-
-        holder.render(superHero)
-
-        compareScreenshot(holder.itemView, R.dimen.super_hero_row_height.toDim())
-    }
-
-    @Test
-    fun showsSuperHeroesWithLongDescriptions() {
-        val superHero = givenASuperHeroWithALongDescription()
-        val holder = givenASuperHeroViewHolder()
-
-        holder.render(superHero)
-
-        compareScreenshot(holder.itemView, R.dimen.super_hero_row_height.toDim())
-    }
-
-    @Test
-    fun showsAvengersBadge() {
-        val superHero = givenASuperHero(isAvenger = true)
-        val holder = givenASuperHeroViewHolder()
-
-        holder.render(superHero)
-
-        compareScreenshot(holder.itemView, R.dimen.super_hero_row_height.toDim())
-    }
+    
 
     private fun givenASuperHeroViewHolder(): SuperHeroViewHolder {
         val context = getInstrumentation().targetContext
@@ -60,32 +31,6 @@ class SuperHeroViewHolderTest : ScreenshotTest {
             view,
             mock<SuperHeroesPresenter>(SuperHeroesPresenter::class.java)
         )
-    }
-
-    private fun givenASuperHeroWithALongDescription(): SuperHero {
-        val superHeroName = "Super Hero Name"
-        val superHeroDescription = """
-            |Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            |incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            |ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            |voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            |proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            |""".trimMargin()
-        val isAvenger = false
-        return givenASuperHero(superHeroName, superHeroDescription, isAvenger)
-    }
-
-    private fun givenASuperHeroWithALongName(): SuperHero {
-        val superHeroName = """
-            |Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            |incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            |ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            |voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            |proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            |""".trimMargin()
-        val superHeroDescription = "Description Super Hero"
-        val isAvenger = false
-        return givenASuperHero(superHeroName, superHeroDescription, isAvenger)
     }
 
     private fun givenASuperHero(
